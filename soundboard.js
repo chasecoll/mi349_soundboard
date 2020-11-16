65508072d7a110b62d6bc929ebcbecfc242094ad
chasecoll <62971935+chasecoll@users.noreply.github.com>
2020-11-15 20:53:04 -0500

    Add files via upload

--- soundboard.js
+++ soundboard.js
@@ -1,10 +1,7 @@
-<audio id="mySound1">
-    <source src="Cat.mp3" type="audio/mpeg">
-    Your browser does not support the audio element.
-  </audio>
- 
-var a = document.getElementById("mySound1"); 
 
-function playSound1() { 
+ 
+var a = new Audio("cat.mp3"); 
+
+document.onclick = playSound1() { 
   a.play(); 
 } 
