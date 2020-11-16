d6c0b74e1faa60d53f530c074356560e75616fa1
chasecoll <62971935+chasecoll@users.noreply.github.com>
2020-11-15 21:34:16 -0500

    Add files via upload

--- soundboard.js
+++ soundboard.js
@@ -1,9 +1,17 @@
 
  
-var audio = document.getElementById("catsound");
+var audio1 = document.getElementById("catsound");
 function soundplay(){
-    audio.play();
+    audio1.play();
 }
 
+var audio2 = document.getElementById("dogsound");
+function soundplay2(){
+    audio2.play();
+}
 
+var audio3 = document.getElementById("birdsound");
+function soundplay3(){
+    audio3.play();
+}
 
